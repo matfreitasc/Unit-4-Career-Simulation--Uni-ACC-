e8793d56b12a5208c015eb3389487459ba484c66
Matheus Cristofoli <matfrietasc@gmail.com>
2024-04-11 23:13:06 -0400

    updated backend for better

--- server/src/routes/cart.js
+++ server/src/routes/cart.js
@@ -3,7 +3,7 @@ const router = express.Router()
 
 const cartController = require('../controllers/cartController')
 
-router.route('/').post(cartController.createCartHandler)
+router.route('/').get(cartController.createCartHandler)
 
 router.route('/:id').post(cartController.updateCartHandler).delete(cartController.deleteCartHandler)
 
