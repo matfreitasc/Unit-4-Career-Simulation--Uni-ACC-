7cf4fdd1066836ba372843d7d43cbc872c8790c6
Matheus Cristofoli <matfrietasc@gmail.com>
2024-04-14 23:40:58 -0400

    urgh another minor bug. access token is now inside the user

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -129,8 +129,8 @@ const refreshToken = async (req, res) => {
                 first_name: user.first_name,
                 last_name: user.last_name,
                 is_admin: user.is_admin,
+                access_token: accessToken,
             },
-            access_token: accessToken,
         })
     })
 }
